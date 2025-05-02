227735610dae9a753da9cd3b9e3bcaf181671ac7
Subbu0717 <subbujakkula1123@gmail.com>
2025-05-02 14:22:13 +0530

    Create switch.dart

---
+++ lab3/switch.dart
@@ -0,0 +1,15 @@
+class switchdemo{
+	void sdemo(String name){
+		switch(name){
+			case 'apple':
+				print("apples are listed");
+			case 'orange':
+				print("oranges are listed");
+			case 'mango':
+				print("mango are listed");
+				break;
+			default:
+				print("The fruit is not listed");
+		}
+	}	
+}
